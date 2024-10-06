7d719ded47dcc499377a6dc92e090f1f05683be4
Hazel Baldava <hazelbaldava016@gmail.com>
2024-10-06 11:33:41 +0800

    finished

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
 
-  // Function to show the contact info popup
+function scrollToSection(sectionId) {
+  document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
+
 function showContactInfo() {
-  document.getElementById('contactPopup').style.display = 'flex'; // Show popup
+  document.getElementById('contactPopup').style.display = 'flex';
 }
 
-// Function to toggle the menu popup
-function toggleMenu() {
-  const popup = document.getElementById('contactPopup');
-  popup.style.display = (popup.style.display === 'flex') ? 'none' : 'flex'; // Toggle display
-}
 function hideContactInfo() {
-    document.getElementById('contactPopup').style.display = 'none';
+  document.getElementById('contactPopup').style.display = 'none';
 }
