07ac3543463ff5f1e6b55bf345aaabc167361bf3
Hazel Baldava <hazelbaldava016@gmail.com>
2024-11-11 00:34:55 +0800

    revision
    initial

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+
+// Smooth open/close toggle for menu
 function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+
+  if (menu.classList.contains("open")) {
+    menu.style.maxHeight = menu.scrollHeight + "px";
+  } else {
+    menu.style.maxHeight = "0";
+  }
+}
+ {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
